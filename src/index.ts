19c7f381dd7c0d4889b7a749d5c30271b9d3f5d4
Narayan Gautam <ng4111894@gmail.com>
2025-07-31 16:41:40 +0545

    Basic runnable grapgql api using graphql-http

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,31 @@
-// import express from 'express';
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.use(express.json());
-
-// app.get('/', (_req, res) => {
-//   res.send('Hello, TypeScript + Express!');
-// });
-
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running at http://localhost:${PORT}`);
-// });
-
-// src/index.ts
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { createHandler } from 'graphql-http/lib/use/express';
 import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@as-integrations/express5';
-
-// Import our schema and resolvers
-import { typeDefs } from './schemas/book.schema';
-import { resolvers } from './resolvers/book';
-
-
-async function startServer() {
-  const app = express();
-  const httpServer = http.createServer(app);
-
-  // Set up Apollo Server
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-
-  await server.start();
-
-  app.use(
-    '/graphql',
-    cors<cors.CorsRequest>(),
-    express.json(),
-    expressMiddleware(server)
-  );
-
-  const PORT = 3000;
-  await new Promise<void>((resolve) => httpServer.listen({ port: PORT }, resolve));
-  console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
-}
-
-startServer();
+ 
+// Construct a schema
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      hello: { 
+        type: GraphQLString,
+        resolve: () => 'Hello world!'
+      },
+    },
+  }),
+});
+ 
+const app = express();
+ 
+// Create and use the GraphQL handler.
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+  }),
+);
+ 
+// Start the server at port
+const PORT = 3000
+app.listen(PORT);
+console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`);
