d1ee3ac2ba44b41db5b5388b010b4e816b2a40ad
Narayan Gautam <ng4111894@gmail.com>
2025-08-01 11:56:10 +0545

    Attach user querying query in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,28 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+
 import { createHandler } from 'graphql-http/lib/use/express';
 import express from 'express';
 import { ruruHTML } from 'ruru/server';
- 
-// Construct a schema
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      hello: { 
-        type: GraphQLString,
-        resolve: () => 'Hello world!'
-      },
-    },
-  }),
-});
+ import {UserSchema} from './schemas/user.schema'
+
  
 const app = express();
 
-// Serve the GraphiQL IDE.
+// // Serve the GraphiQL IDE.
 app.get('/', (_req, res) => {
   res.type('html');
   res.end(ruruHTML({ endpoint: '/graphql' }));
 });
  
-// Create and use the GraphQL handler.
+
 app.all(
   '/graphql',
   createHandler({
-    schema: schema,
+    schema:UserSchema
+    
+
   }),
 );
- 
+
 // Start the server at port
 const PORT = 3000
 app.listen(PORT);
